49586445e62dd6f119f5119ce93737d3d5f6dfd5
Salvador Osuna-Caballero <95479812+SalvaOsuna@users.noreply.github.com>
2021-12-15 12:43:14 +0100

    summary

--- script/explore data CC.R
+++ script/explore data CC.R
@@ -8,18 +8,13 @@ library(metan)
 
 df_cc <- data.frame(Rust_controlled_condition)
 
-gen_alpha_cc <- gamem(df_cc, GEN, REP, c(AUDPC,PL50,IF), block = BLOCK)
-gen_alpha_cc2 <- gamem(df_cc, GEN, REP, c(AUDPC,PL50,IF))
+gen_alpha_cc <- gamem(df_cc, GEN, REP, AUDPC_n, block = BLOCK)
 
 get_model_data(gen_alpha_cc)
-get_model_data(gen_alpha_cc2)
+get_model_data(gen_alpha_cc,  "lrt")
+plot(gen_alpha_cc)
+plot(gen_alpha_cc, type = "re")
 
 hist(gen_alpha_cc$AUDPC$BLUPgen$Predicted)
-hist(df_cc$AUDPC)
-
-hist(gen_alpha_cc$PL50$BLUPgen$Predicted)
-hist(df_cc$PL50)
-
-hist(gen_alpha_cc$IF$BLUPgen$Predicted)
-hist(df_cc$IF)
+hist(df_cc$AUDPC_n)
 
